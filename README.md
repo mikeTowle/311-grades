fab3bfafebbcd623cf51911a55d1eed458431689
Shiv <ansalshiv@gmail.com>
2019-10-21 04:23:33 -0400

    add score func

--- README.md
+++ README.md
@@ -2,13 +2,19 @@
 
 python grades.py <A> <C>
 
+python grades.py <A> <C> <Score>
 
-Example: 
-python grades.py 84 52 -> 
+Example:
+
+python grades.py 84 50 55 ->
+
 A : 84
 A-: 79
-B+: 73
-B : 68
-B-: 63
-C+: 57
-C : 52
+B+: 72
+B : 67
+B-: 62
+C+: 55
+C : 50
+
+Your Score: 55 = C
+
--- grades.py
+++ grades.py
@@ -1,28 +1,50 @@
 
 
-import sys, getopt
+import sys, collections
+
+
+def determine_grade(a,c,score):
+	grades = calculate(a,c)
+	for key in sorted(grades.keys()):
+		if(score >= grades[key]):
+			print("\nYour Score: " + str(score) + " = " + key)
+			break
+
 
 
 def calculate(a, c):
 	difference = ((a-c)/2)
 	third = difference/3
 	b = c + difference
+	grades = collections.OrderedDict()
 
 	print("A : " + str(a))
+	grades["A"] = a
 	print("A-: " + str(a-third))
+	grades["A-"] = a-third
 	print("B+: " + str(b+third))
+	grades["B+"] = b+third
 	print("B : " + str(b))
+	grades["B"] = b
 	print("B-: " + str(b-third))
+	grades["B-"] = b-third
 	print("C+: " + str(c+third))
+	grades["C+"] = c+third
 	print("C : " + str(c))
-
+	grades["C"] = c
+	return grades
 
 def main(argv):
-	if(len(argv) != 2):
+
+	if(len(argv) == 2):
+		calculate(int(float(argv[0])), int(float(argv[1])))
+	elif(len(argv) == 3):
+		determine_grade(int(float(argv[0])), int(float(argv[1])), int(float(argv[2])))
+	else:
 		print 'grades.py <A> <C>'
 		sys.exit()
 
-	calculate(int(argv[0]), int(argv[1]))
+	
 
 
 if __name__ == "__main__":
